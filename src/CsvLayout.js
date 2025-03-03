4a4d601bd9bad5376aaaae9a1fb3ba84b19788e3
amit kumar gupta <amitgupta.gwl@gmail.com>
2025-03-03 10:16:41 +0530

    Fix CSV layout for allsupported levels

--- src/CsvLayout.js
+++ src/CsvLayout.js
@@ -4,34 +4,58 @@ export default class CsvLayout{
   constructor(){
   }
 
-  trace(logRecord){
-    let msg = `${logRecord.id},${logRecord.reportTime},${logRecord.flowName}`;
-    if(logRecord.parentFlowId){
-      return `${msg},${logRecord.parentFlowId},${logRecord.parentStepId}`;
-    }else{
-      return msg;
-    }
-  }
+
   info(lr){
-    let msg = `${lr.success},${lr.id},${lr.reportTime},${lr.flowName},${lr.version},${stringify(lr.steps)},${Date.now()-lr.reportTime}`; 
-    if(lr.parentFlowId){
-      msg += `,${lr.parentFlowId},${lr.parentStepId}`;
+    let msg = "";
+    if(!lr.steps){
+      msg = `HEAD,${lr.id},${lr.reportTime},${lr.flowName},${lr.version}`;
+      if(lr.parentFlowId){
+        return `${msg},${lr.parentFlowId},${lr.parentStepId}`;
+      }
     }else{
-      msg += ",,";
+      msg = `FLOW,${lr.id},${lr.reportTime},${lr.flowName},${lr.version},${stringify(lr.steps)},${Date.now()-lr.reportTime},${lr.success}`; 
+      if(lr.parentFlowId){
+        msg += `,${lr.parentFlowId},${lr.parentStepId}`;
+      }else{
+        msg += ",,";
+      }
+      msg = `,${lr.errMsg}`
     }
-    msg = `,${lr.errMsg}`
     return msg;
   }
-  debug(logRecord){
-    return logRecord
+  debug(lr){
+    let msg = `DEBUG,${lr.id},${lr.reportTime},${lr.lastStepId},${lr.msg},${stringify(lr.data)}`; 
+    return msg;
   }
-  warn(logRecord){
-    logRecord
+  trace(lr){
+    const data = this.#transformStackTraceForCSV(lr.data);
+    let msg = `TRACE,${lr.id},${lr.reportTime},${lr.lastStepId},${lr.msg},${data}`; 
+    return msg;
   }
-  error(logRecord){
-    logRecord
+  warn(lr){
+    let msg = `WARN,${lr.id},${lr.reportTime},${lr.lastStepId},${lr.msg},${stringify(lr.data)}`; 
+    return msg;
   }
-  fatal(logRecord){
-    logRecord
+  error(lr){
+    let msg = `ERROR,${lr.id},${lr.reportTime},${lr.lastStepId},${lr.msg},${stringify(lr.data)}`; 
+    return msg;
+  }
+  fatal(lr){
+    let msg = `FATAL,${lr.id},${lr.reportTime},${lr.lastStepId},${lr.msg},${stringify(lr.data)}`; 
+    return msg;
+  }
+  
+  #transformStackTraceForCSV(stackTrace) {
+    // Split the stack trace into lines and skip the first line (e.g., "Error: ...")
+    const lines = stackTrace.split('\n').slice(1);
+  
+    // Take only the first two meaningful lines
+    const relevantLines = lines.slice(0, 2); //TODO: Make it configurable
+  
+    // Flatten the lines into a single string using a delimiter (e.g., '|')
+    const flattenedTrace = relevantLines.join('|');
+  
+    // Escape double quotes and enclose in double quotes for CSV compatibility
+    return `"${flattenedTrace.replace(/"/g, '""')}"`;
   }
 }
